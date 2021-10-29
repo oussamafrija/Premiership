4aa21f9355bb5008113d2d89441d9715d82eb168
oussama frija <82705125+oussamafrija@users.noreply.github.com>
2021-10-29 22:59:03 +0100

    Add files via upload

---
+++ my_test/my_test/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace my_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nud_ht = new System.Windows.Forms.NumericUpDown();
+            this.nud_at = new System.Windows.Forms.NumericUpDown();
+            this.cb_ht = new System.Windows.Forms.ComboBox();
+            this.cb_at = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_ht)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_at)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(108, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Empty All Text";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(21, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Home Team";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(21, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Away Team";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(314, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Score";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(314, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 19);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Score";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.MediumOrchid;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 138);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 70;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(486, 400);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // nud_ht
+            // 
+            this.nud_ht.Location = new System.Drawing.Point(387, 13);
+            this.nud_ht.Name = "nud_ht";
+            this.nud_ht.Size = new System.Drawing.Size(120, 23);
+            this.nud_ht.TabIndex = 12;
+            // 
+            // nud_at
+            // 
+            this.nud_at.Location = new System.Drawing.Point(387, 57);
+            this.nud_at.Name = "nud_at";
+            this.nud_at.Size = new System.Drawing.Size(120, 23);
+            this.nud_at.TabIndex = 13;
+            // 
+            // cb_ht
+            // 
+            this.cb_ht.FormattingEnabled = true;
+            this.cb_ht.Items.AddRange(new object[] {
+            "",
+            "Arsenal",
+            "Aston Villa",
+            "Brentford",
+            "Brighton & Hove Albion",
+            "Burnley",
+            "Chelsea",
+            "Crystal Palace",
+            "Everton",
+            "Leeds United",
+            "Leicester City",
+            "Liverpool",
+            "Manchester city",
+            "Manchester UTD",
+            "Newcastle United",
+            "NORNorwich City",
+            "Southampton",
+            "Tottenham Hotspur",
+            "Watford",
+            "West Ham United",
+            "Wolverhampton FC"});
+            this.cb_ht.Location = new System.Drawing.Point(115, 12);
+            this.cb_ht.Name = "cb_ht";
+            this.cb_ht.Size = new System.Drawing.Size(170, 23);
+            this.cb_ht.TabIndex = 14;
+            // 
+            // cb_at
+            // 
+            this.cb_at.FormattingEnabled = true;
+            this.cb_at.Items.AddRange(new object[] {
+            "",
+            "Arsenal",
+            "Aston Villa",
+            "Brentford",
+            "Brighton & Hove Albion",
+            "Burnley",
+            "Chelsea",
+            "Crystal Palace",
+            "Everton",
+            "Leeds United",
+            "Leicester City",
+            "Liverpool",
+            "Manchester city",
+            "Manchester UTD",
+            "Newcastle United",
+            "NORNorwich City",
+            "Southampton",
+            "Tottenham Hotspur",
+            "Watford",
+            "West Ham United",
+            "Wolverhampton FC"});
+            this.cb_at.Location = new System.Drawing.Point(115, 56);
+            this.cb_at.Name = "cb_at";
+            this.cb_at.Size = new System.Drawing.Size(170, 23);
+            this.cb_at.TabIndex = 15;
+            // 
+            // button5
+            // 
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(526, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(106, 531);
+            this.button5.TabIndex = 23;
+            this.button5.Text = "New Season, Lets GOOOO";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button6.Location = new System.Drawing.Point(269, 95);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(152, 30);
+            this.button6.TabIndex = 24;
+            this.button6.Text = "Apply ";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(645, 541);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.cb_at);
+            this.Controls.Add(this.cb_ht);
+            this.Controls.Add(this.nud_at);
+            this.Controls.Add(this.nud_ht);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_ht)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_at)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        //private System.Windows.Forms.TextBox txt_rat;
+        //private System.Windows.Forms.TextBox txt_rht;
+        private System.Windows.Forms.NumericUpDown nud_ht;
+        private System.Windows.Forms.NumericUpDown nud_at;
+        private System.Windows.Forms.ComboBox cb_ht;
+        private System.Windows.Forms.ComboBox cb_at;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ my_test/my_test/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace my_test
+{
+    public partial class Form1 : Form
+    {
+        public string S;
+        SqlDataReader dr;
+        SqlConnection cn = new SqlConnection(@"Data Source=DESKTOP-CP58I16\SQLEXPRESS;Initial Catalog=db;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cn.ConnectionString = "Data Source=DESKTOP-CP58I16\\SQLEXPRESS;Initial Catalog=Sport;Integrated Security=True";
+            
+            cn.Open();
+            S = "select * from ranking order by Points desc";
+            SqlCommand cmd = new SqlCommand(S, cn);
+            dr = cmd.ExecuteReader();
+            DataTable t = new DataTable();
+            t.Load(dr);
+            dataGridView1.DataSource = t;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                row.HeaderCell.Value = (row.Index + 1).ToString();
+                row.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            }
+            dataGridView1.Refresh();
+            cn.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cb_ht.SelectedIndex=0;
+            cb_at.SelectedIndex=0;
+            nud_ht.Value = 0;
+            nud_at.Value = 0;            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("update ranking set Points =0, For_=0, Against=0, Difference=0", cn);
+            cmd.ExecuteNonQuery();
+           
+            S = "select * from ranking order by Points desc";
+            SqlCommand cmd1 = new SqlCommand(S, cn);
+            dr = cmd1.ExecuteReader();
+            DataTable t = new DataTable();
+            t.Load(dr);
+            dataGridView1.DataSource = t;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                row.HeaderCell.Value = (row.Index + 1).ToString();
+                row.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            }
+            dataGridView1.Refresh();
+            cn.Close();
+            MessageBox.Show("Start");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (cb_at.Text != cb_ht.Text )
+            {
+                cn.Open();
+                if (nud_at.Value > nud_ht.Value)
+                {
+                SqlCommand cmd = new SqlCommand("update ranking set Points += @po where Teams=@tea ", cn);
+                cmd.Parameters.AddWithValue("@po", 3);
+                cmd.Parameters.AddWithValue("@tea", cb_at.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show(cb_at.Text + " Lost \n" + cb_ht.Text + " Won " );
+                }
+                else if(nud_at.Value < nud_ht.Value)
+                {
+                SqlCommand cmd = new SqlCommand("update ranking set Points += @po where Teams=@tea ", cn);
+                cmd.Parameters.AddWithValue("@po", 3);
+                cmd.Parameters.AddWithValue("@tea", cb_ht.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show(cb_ht.Text + " Won \n" + cb_at.Text + " Lost ");
+                }
+                else if (nud_at.Value == nud_ht.Value)
+                {
+                SqlCommand cmd = new SqlCommand("update ranking set Points += @po where Teams=@tea ", cn);
+                cmd.Parameters.AddWithValue("@po", 1);
+                cmd.Parameters.AddWithValue("@tea", cb_at.Text);
+                cmd.ExecuteNonQuery();
+
+                SqlCommand cmd2 = new SqlCommand("update ranking set Points += @po where Teams=@tea ", cn);
+                cmd2.Parameters.AddWithValue("@po", 1);
+                cmd2.Parameters.AddWithValue("@tea", cb_ht.Text);
+                cmd2.ExecuteNonQuery();
+                MessageBox.Show(cb_ht.Text + " And " + cb_at.Text + "Draw");
+                }
+
+                SqlCommand cmd4 = new SqlCommand("update ranking set Against += @po1 where Teams=@tea1 ", cn);
+                cmd4.Parameters.AddWithValue("@po1", nud_at.Value);
+                cmd4.Parameters.AddWithValue("@tea1", cb_at.Text);
+                cmd4.ExecuteNonQuery();
+
+                SqlCommand cmd3 = new SqlCommand("update ranking set For_ += @po where Teams=@tea ", cn);
+                cmd3.Parameters.AddWithValue("@po", nud_ht.Value);
+                cmd3.Parameters.AddWithValue("@tea", cb_ht.Text);
+                cmd3.ExecuteNonQuery();
+                
+                SqlCommand cmd5 = new SqlCommand("update ranking set Difference += @dif where Teams=@tea ", cn);
+                cmd5.Parameters.AddWithValue("@dif", nud_ht.Value-nud_at.Value );
+                cmd5.Parameters.AddWithValue("@tea", cb_ht.Text);
+                cmd5.ExecuteNonQuery();
+
+                SqlCommand cmd6 = new SqlCommand("update ranking set Difference += @dif where Teams=@tea1 ", cn);
+                cmd6.Parameters.AddWithValue("@dif", nud_at.Value - nud_ht.Value);
+                cmd6.Parameters.AddWithValue("@tea1", cb_at.Text);
+                cmd6.ExecuteNonQuery();
+
+                S = "select * from ranking order by Points desc";
+                 SqlCommand cmd1 = new SqlCommand(S, cn);
+                  dr = cmd1.ExecuteReader();
+                   DataTable t = new DataTable();
+                   t.Load(dr);
+                    dataGridView1.DataSource = t;
+                   foreach (DataGridViewRow row in dataGridView1.Rows)
+                    {
+                row.HeaderCell.Value = (row.Index + 1).ToString();
+                row.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                     }
+            dataGridView1.Refresh();
+            cn.Close();
+            }
+            else
+            {
+                MessageBox.Show("That's impossible");
+            }
+        }
+    }
+}
